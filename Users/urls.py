8d61f612b9ee46c902f83bf3ca65f4fc3340a0e7
yacn1414 <yacn1214@gmail.com>
2022-09-10 00:54:43 +0430

    user fornt half

--- Users/urls.py
+++ Users/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
 from . import views
 urlpatterns = [
-    path('login',views.login),
+    path('login',views.auth,name="auth"),
     path('signup',views.signup),
     
 ]
--- Users/views.py
+++ Users/views.py
@@ -1,9 +1,8 @@
-
 from django.shortcuts import render
-
 from django.http import HttpResponse
+from django.contrib.auth.forms import AuthenticationForm
 # Create your views here.
-def login(request):
-    pass
-def signup(request):
+def auth(request):
     return HttpResponse("ok")
+def signup(request):
+    return render(request,'user.html',{"mmd":AuthenticationForm})
