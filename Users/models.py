0bdfc1dba9ce1d537e2f271400abfa0a624f1b0f
yacn1414 <yacn1214@gmail.com>
2022-09-12 21:27:23 +0430

    Master

--- Users/models.py
+++ Users/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+class User(models.Model):
+    name = models.CharField(max_length=255,blank=False,null=False)
+    phone = models.IntegerField(blank=False,null=False)
+    last_seen_site = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.name
+
+    def __unicode__(self):
+        return self.phone
--- Users/views.py
+++ Users/views.py
@@ -1,8 +1,14 @@
+# from .models import User
+from django.contrib.auth.models import User
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse,HttpResponseRedirect
 from django.contrib.auth.forms import AuthenticationForm
 # Create your views here.
 def auth(request):
-    return HttpResponse("ok")
+    if request.method == "POST":
+        name = str(request.POST['name'])
+        phone = int(request.POST['phone'])
+    else:
+        return HttpResponseRedirect("../signup")
 def signup(request):
     return render(request,'user.html',{"mmd":AuthenticationForm})
